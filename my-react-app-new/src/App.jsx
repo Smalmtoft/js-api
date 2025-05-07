b1919ff70517ddb309429865be47a94a89800ec0
Sandra Malmtoft <sandra.malmtoft@gmail.com>
2025-05-07 08:59:16 +0200

    cute dogs API used

--- my-react-app-new/src/App.jsx
+++ my-react-app-new/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import DogGame from './DogGame';
 
 function App() {
+  return (
+    <div className="dogGame">
+      <DogGame />
+    </div>
+  );
 }
 
-export default App
+export default App;
